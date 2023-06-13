b8bd2195af916f33c97f3c097c4ee906e259278a
RubinaBallach <rubina.ballach@gmail.com>
2023-06-13 17:54:31 +0200

    calendar finished

--- DatesProject_Rubina.py
+++ DatesProject_Rubina.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta, time, timezone
-import calendar # Import neededs Modules
+import calendar 
+import pytz
+# Import needed Modules
 #Design User Input Menu
 menu = input("""We're all about time today. What to you want to do?
 [1] What time is it?)
@@ -12,8 +14,10 @@ menu = input("""We're all about time today. What to you want to do?
 [8] What time is it on the other side of the planet
 [9] Tell me a joke
 [10] Surprise: \n\t\n""")
+#Variables to use in several functions            
 current_time = datetime.now()
 current_year = int(datetime.strftime(current_time, "%Y"))
+current_month = int(datetime.strftime(current_time,"%m"))
 #Define menu options
 def menu_1() : # current time, nicely formatted
     print(f"It is {current_time.strftime('%A,%d.%B.%Y, %H:%M:%S')}.")
@@ -56,7 +60,25 @@ def menu_5 ():
     elif choice == "3":
         print (f"The number of years between {t1} and {t2} is {round(tdelta.days/365.25)} years")
     else:
-        print("Please choose a valid format.")    
+        print("Please choose a valid format.")
+#display calendar        
+def menu_6():
+    print("Do you want to see this months calendar or another time?")
+    choice = input(" Choose 'now' for the current month or type a date in the folling format 'yyyy-mm': ")
+    if choice == "now":
+        print(f"Year: {current_year}"+"\t"+f"Month:{current_month}")
+        print(calendar.month(current_year, current_month))
+    elif choice != "now":
+        mcalendar = datetime.strptime(choice, "%Y-%m")
+        print(f"Year: {mcalendar.year}"+"\t"+f"Month:{mcalendar.month}")
+        print(calendar.month(int(mcalendar.year), int(mcalendar.month)))
+    else:
+        print("Please choose a valid option!")
+#Timezone Conversion
+def menu_7():
+
+
+
 #Create Menu Tree as an if/else condition                 
 if menu == "1":
     menu_1()
@@ -68,5 +90,9 @@ elif menu == "4":
     menu_4()
 elif menu == "5":         
     menu_5()
+elif menu == "6":
+    menu_6()
+elif menu == "7":
+    menu_7()        
 else:
     print("Please make a valid choice.")    
