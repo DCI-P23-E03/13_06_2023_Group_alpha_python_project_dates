b1fb962ea06824bbdf2fde9e9f6047f6fa040408
sbrener1610 <sbrener1610@gmail.com>
2023-06-13 15:39:03 +0200

    First few tasks

---
+++ SB_convert.py
@@ -0,0 +1,12 @@
+import datetime
+
+
+flag=0
+while (flag==0):
+    input_date = input("Enter a date (yyyy/mm/dd): ")
+    try:
+        Some_date=datetime.datetime.strptime(input_date,"%Y/%m/%d")
+        flag=1
+    except ValueError:
+        print("Invalid date")
+print(f"I converted {input_date} to {Some_date}")
